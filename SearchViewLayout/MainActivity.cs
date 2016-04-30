919f88e9c7ad3934c9e93f158c419d58b02312eb
Peter Major <peter@petermajor.co.uk>
2016-04-30 12:43:00 +0100

    first commit

---
+++ SearchViewLayout/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+
+namespace SearchViewLayout
+{
+	[Activity (Label = "SearchViewLayout", MainLauncher = true)]
+	public class MainActivity : AppCompatActivity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			var toolbar = FindViewById<Toolbar> (Resource.Id.toolbar);
+			SetSupportActionBar (toolbar);
+		}
+	}
+}
